2e45e05ae384537da3f5b631827dd5ff80265cca
SeoYoon <34805713+s-y00n3@users.noreply.github.com>
2024-01-12 17:52:48 +0900

    Database 연결
    메모리 저장이 아닌 h2 Database로 연결해서 데이터 저장 / 조회

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,38 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+/*
+ * 스프링 뜰 때 @Configuration 읽고,
+ * @Bean을 스프링 빈에 등록하는 거라고 인식함
+ * */
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
+    private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean // 스프링 빈을 등록할 거임
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
